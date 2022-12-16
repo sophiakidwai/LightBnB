a61d0c12e7b10f34381954028e4b601839fe2474
sophia kidawai <sophia.kidwai@telus.com>
2022-12-15 17:49:21 -0800

    updated getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -73,7 +73,7 @@ const addUser = function(user) {
     });
 
  };
- 
+
 exports.addUser = addUser;
 
 /// Reservations
@@ -84,8 +84,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+
+  const sqlQuery = `SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+  
+    return pool.query(sqlQuery, [guest_id, limit])
+    .then((result) => {
+        console.log(result.rows);
+        return result.rows;
+      })
+    .catch((err) => {
+        console.log(err.message);
+      });
+   };
 exports.getAllReservations = getAllReservations;
 
 /// Properties
